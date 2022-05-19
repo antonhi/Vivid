ab6b3911866870ec23eec2498b20e43886938946
antonhorvath <aih180000@utdallas.edu>
2022-05-19 12:25:57 -0500

    ButtonIconComponent Created

---
+++ lib/components/buttons/button_icon_component.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/cupertino.dart';
+
+class ButtonIconComponent extends StatelessWidget {
+
+  final Color buttonColor;
+  final Icon icon;
+  final double height, iconPadding = 10;
+  final void Function() onTap;
+
+  const ButtonIconComponent({Key? key,
+    required this.buttonColor,
+    required this.icon,
+    required this.height,
+    required this.onTap}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        height: height,
+        decoration: BoxDecoration(
+            color: buttonColor,
+            borderRadius: BorderRadius.circular(height/2)
+        ),
+        child: Align(
+          alignment: Alignment.center,
+          child: icon,
+        ),
+      ),
+    );
+  }
+}
